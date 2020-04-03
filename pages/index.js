defaaa19350241b5baf100125947255beaecbb1c
Lakhveer Bawa <bawa93lakhveer@gmail.com>
2020-04-03 21:26:32 +0530

    express framework added

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,26 @@
 import Layout  from '../components/Layout.js';
 import ReduxDemo  from '../components/ReduxDemo.js';
+import styled from "styled-components";
 
 class Home extends React.Component {
   render() {
+
+      const Rocket = styled.div`
+  text-align: center;
+  img {
+    width: 630px;
+  }
+`;
      return (
-        <div>
-        
+        <Rocket>
         <Layout>
            <h1>This is Homepage</h1>
            <ReduxDemo></ReduxDemo>
+            <img src="https://media.giphy.com/media/QbumCX9HFFDQA/giphy.gif" />
+
         </Layout>
-           
-        </div>
+
+        </Rocket>
      );
   }
 }
